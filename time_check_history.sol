aed167d403bda637ce10eb188b417d5bca2de105
Maurilio Santana <cabelotaina@gmail.com>
2018-09-23 13:37:37 -0300

    update contract

--- time_check_history.sol
+++ time_check_history.sol
@@ -2,35 +2,31 @@ pragma solidity ^0.4.25;
 
 contract TimeCheck{
 	address private _owner = msg.sender;
-	mapping (uint256 => DayWork) private dayWorkHistory;
-	uint256 private counter;
-
+	
 	struct DayWork {
 		uint256 timestamp;
 		string hash;
 	}
 	
-	constructor() public {
-	    counter = 0;
+	struct AccountInfo { 
+	  mapping (uint256 => DayWork) dayWorkHistory;
+	  uint256 counter;
+	}
+	
+	mapping (address => AccountInfo) private workers;
+
+	function size() public returns (uint256) {
+	    return workers[msg.sender].counter;
 	}
 
-	modifier onlyOwner {
-	  if(msg.sender != _owner) revert();
-	  _;
+	function getDayOfWork(uint index) public returns (uint256, string) {
+		return (workers[msg.sender].dayWorkHistory[index].timestamp, workers[msg.sender].dayWorkHistory[index].hash);
 	}
 
-	function size() public onlyOwner returns (uint256) {
-	    return counter;
-	}
-
-	function getDayOfWork(uint index) onlyOwner public returns (uint256, string) {
-		return (dayWorkHistory[index].timestamp, dayWorkHistory[index].hash);
-	}
-
-	function addDayOfWork(string hash) onlyOwner public {
-	  counter++;
-	  dayWorkHistory[counter].timestamp = timeCall();
-	  dayWorkHistory[counter].hash = hash;
+	function addDayOfWork(string hash) public {
+	  workers[msg.sender].counter++;
+	  workers[msg.sender].dayWorkHistory[workers[msg.sender].counter].timestamp = timeCall();
+	  workers[msg.sender].dayWorkHistory[workers[msg.sender].counter].hash = hash;
 	}
 
     function timeCall() private returns (uint256){
